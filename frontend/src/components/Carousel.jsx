ac9ef5f696dc18a34da5d9e20be5dfe9eb4dd09d
Sahadipankar <sahadepankar@gmail.com>
2025-10-02 17:35:13 +0530

    Update Carousel component: change image class to include background color for better visibility

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -62,7 +62,7 @@ const Carousel = () => {
                                         <button className='bg-gradient-to-r from-red-500 to-purple-500 text-white px-3 py-2 rounded-md cursor-pointer mt-2'>Shop Now</button>
                                     </div>
                                     <div>
-                                        <img src={item.thumbnail} alt={item.title} className='rounded-full w-[550px] hover:scale-105 transition-all shadow-2xl shadow-red-400' />
+                                        <img src={item.thumbnail} alt={item.title} className='bg-white rounded-full w-[550px] hover:scale-105 transition-all shadow-2xl shadow-red-400' />
                                     </div>
                                 </div>
                             </div>
